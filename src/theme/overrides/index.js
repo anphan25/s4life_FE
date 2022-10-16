26834fa7057f25f07369a4bebbde1e7fbbd88b6e
Nguyen Thi Hoang Dung <dungnguyen09172000@gmail.com>
2022-10-16 13:08:20 +0700

    Fix overrides

--- src/theme/overrides/index.js
+++ src/theme/overrides/index.js
@@ -1,44 +1,43 @@
-import Fab from "./Fab";
-import Card from "./Card";
-import Chip from "./Chip";
-import Tabs from "./Tabs";
-import Menu from "./Menu";
-import List from "./List";
-import Table from "./Table";
-import Alert from "./Alert";
-import Badge from "./Badge";
-import Paper from "./Paper";
-import Input from "./Input";
-import Radio from "./Radio";
-import Drawer from "./Drawer";
-import Dialog from "./Dialog";
-import Avatar from "./Avatar";
-import Rating from "./Rating";
-import Slider from "./Slider";
-import Button from "./Button";
-import Switch from "./Switch";
-import Select from "./Select";
-import SvgIcon from "./SvgIcon";
-import Tooltip from "./Tooltip";
-import Popover from "./Popover";
-import Stepper from "./Stepper";
-import DataGrid from "./DataGrid";
-import Skeleton from "./Skeleton";
-import Backdrop from "./Backdrop";
-import Progress from "./Progress";
-import Timeline from "./Timeline";
-import TreeView from "./TreeView";
-import Checkbox from "./Checkbox";
-import Accordion from "./Accordion";
-import Pagination from "./Pagination";
-import Breadcrumbs from "./Breadcrumbs";
-import ButtonGroup from "./ButtonGroup";
-import CssBaseline from "./CssBaseline";
-import Autocomplete from "./Autocomplete";
-import ToggleButton from "./ToggleButton";
-import ControlLabel from "./FormControlLabel";
-import LoadingButton from "./LoadingButton";
-import InputLabel from "./InputLabel";
+import Fab from './Fab';
+import Card from './Card';
+import Chip from './Chip';
+import Tabs from './Tabs';
+import Menu from './Menu';
+import List from './List';
+import Table from './Table';
+import Alert from './Alert';
+import Badge from './Badge';
+import Paper from './Paper';
+import Input from './Input';
+import Radio from './Radio';
+import Drawer from './Drawer';
+import Dialog from './Dialog';
+import Avatar from './Avatar';
+import Rating from './Rating';
+import Slider from './Slider';
+import Button from './Button';
+import Switch from './Switch';
+import Select from './Select';
+import SvgIcon from './SvgIcon';
+import Tooltip from './Tooltip';
+import Popover from './Popover';
+import Stepper from './Stepper';
+import DataGrid from './DataGrid';
+import Skeleton from './Skeleton';
+import Backdrop from './Backdrop';
+import Progress from './Progress';
+import Timeline from './Timeline';
+import TreeView from './TreeView';
+import Checkbox from './Checkbox';
+import Accordion from './Accordion';
+import Pagination from './Pagination';
+import Breadcrumbs from './Breadcrumbs';
+import ButtonGroup from './ButtonGroup';
+import CssBaseline from './CssBaseline';
+import Autocomplete from './Autocomplete';
+import ToggleButton from './ToggleButton';
+import ControlLabel from './FormControlLabel';
+import LoadingButton from './LoadingButton';
 
 export function Overrides(theme) {
   return Object.assign(
@@ -81,7 +80,6 @@ export function Overrides(theme) {
     Autocomplete(theme),
     ControlLabel(theme),
     ToggleButton(theme),
-    LoadingButton(theme),
-    InputLabel(theme)
+    LoadingButton(theme)
   );
 }
