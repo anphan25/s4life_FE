655611524c0a97063750518cc080bda9f3e7e75c
An <sajno076@gmail.com>
2022-10-19 23:12:06 +0700

    [AnPDT]submit form login, config axios and redix

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -1,1 +1,9 @@
+import axiosInstance from 'config/axiosConfig';
+
+const apiPath = '/auth';
+
 export function getAccessToken(refreshToken) {}
+
+export async function loginUserPassword(params) {
+  return await axiosInstance.post(apiPath + '/login', params);
+}
--- src/app/slices/AuthSlice.js
+++ src/app/slices/AuthSlice.js
@@ -1,14 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import jwtDecode from 'jwt-decode';
 
-const initialState = {
-  // error: "",
-  // isLoading: false,
-  // auth: getAuthState(),
-  auth: null,
+const getAuthState = () => {
+  const storedValue = localStorage.getItem('authTokens');
+  const authTokens = storedValue ? JSON.parse(storedValue) : null;
+  if (authTokens)
+    return {
+      ...authTokens,
+      user: jwtDecode(authTokens.accessToken),
+    };
+  else return null;
 };
 
-export const authSlice = createSlice({ name: "auth", initialState });
+const initialState = {
+  error: '',
+  isLoading: false,
+  auth: getAuthState(),
+};
 
-export const {} = authSlice.actions;
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    authPending: (state) => {
+      state.isLoading = true;
+    },
+    clearMessage: (state) => {
+      state.error = '';
+    },
+    loginSuccess: (state, action) => {
+      state.auth = action.payload;
+      state.isLoading = false;
+    },
+    loginFail: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    logoutSuccess: (state) => {
+      state.auth = null;
+      localStorage.removeItem('authTokens');
+    },
+    setToken: (state, action) => {
+      state.auth.accessToken = action.payload;
+      state.auth.user = jwtDecode(action.payload);
+      localStorage.setItem(
+        'authTokens',
+        JSON.stringify({
+          accessToken: state.auth.accessToken,
+          refreshToken: state.auth.refreshToken,
+        })
+      );
+    },
+    refreshFail: (state, action) => {
+      state.error = action.payload;
+      state.auth = null;
+      localStorage.removeItem('authTokens');
+    },
+  },
+});
+
+export const { authPending, loginFail, loginSuccess, logoutSuccess, setToken, refreshFail, clearMessage } =
+  authSlice.actions;
 
 export default authSlice.reducer;
