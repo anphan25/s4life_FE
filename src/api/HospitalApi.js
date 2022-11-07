7aaf1e557b63596d931ac5128a20b495fee27626
An <sajno076@gmail.com>
2022-11-07 23:22:38 +0700

    Add more columns in csv file, fix snackbar display

--- src/api/HospitalApi.js
+++ src/api/HospitalApi.js
@@ -4,10 +4,8 @@ import queryString from 'query-string';
 const apiPath = '/hospitals';
 
 export async function importCSVHospitalData(params) {
-  console.log('params: ', params);
   return await axiosInstance.post(apiPath + '/import', params);
 }
 export async function getHospitalsList(params) {
-  console.log('params: ', params);
   return await axiosInstance.get(`${apiPath}?${queryString.stringify(params)}`);
 }
--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_API_URL,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Type': 'application/json',
   },
 });
 
