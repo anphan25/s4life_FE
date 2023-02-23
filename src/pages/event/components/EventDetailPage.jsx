da167d270df211e5dbba989ef2906e53b471e1c1
An <sajno076@gmail.com>
2023-02-23 17:15:53 +0700

    Add skeleton loading for detail event

--- src/pages/event/components/EventDetailPage.jsx
+++ src/pages/event/components/EventDetailPage.jsx
@@ -12,6 +12,7 @@ import {
   Chip,
   DialogActions,
   Button,
+  Skeleton,
 } from '@mui/material';
 import { HeaderBreadcumbs, CustomSnackBar, CustomDialog, Icon } from 'components';
 import moment from 'moment';
@@ -52,6 +53,10 @@ const HeaderMainStyle = styled(Stack)(({ theme }) => ({
 }));
 
 const EventImageStyle = styled(Box)(({ theme }) => ({
+  width: '100%',
+  height: 'auto',
+  maxHeight: '500px',
+
   '& .event-img': {
     width: '100%',
     height: 'auto',
@@ -123,7 +128,7 @@ const DialogButtonGroup = styled(DialogActions)(({ theme }) => ({
 }));
 
 const EventDetailPage = () => {
-  const [detailData, setDetailData] = useState();
+  const [detailData, setDetailData] = useState(null);
   const [cancelEventId, setCancelEventId] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [isButtonLoading, setIsButtonLoading] = useState(false);
@@ -378,123 +383,190 @@ const EventDetailPage = () => {
           </Stack>
 
           <EventImageStyle>
-            <img
-              className="event-img"
-              src={detailData?.eventImages[0]?.imageUrl || DEFAULT_EVENT_IMAGE_URL}
-              alt="Ảnh sự kiện"
-            />
+            {detailData ? (
+              <img
+                className="event-img"
+                src={detailData?.eventImages[0]?.imageUrl || DEFAULT_EVENT_IMAGE_URL}
+                alt="Ảnh sự kiện"
+              />
+            ) : (
+              <Skeleton variant="rounded" sx={{ maxHeight: '500px', width: '100%', height: '500px' }} />
+            )}
           </EventImageStyle>
 
-          <Box>
-            <Typography
-              sx={{
-                fontSize: '35px',
-                fontWeight: 'bold',
-                wordBreak: 'break-all',
-              }}
-            >
-              {detailData?.name}
-            </Typography>
+          {detailData ? (
+            <Box>
+              <Typography
+                sx={{
+                  fontSize: '35px',
+                  fontWeight: 'bold',
+                  wordBreak: 'break-all',
+                }}
+              >
+                {detailData?.name}
+              </Typography>
 
-            <Stack direction="row" sx={{ marginTop: '10px' }}>
-              <StatusTagStyle label={detailData?.status} />
-              {detailData?.isEmergency && <EmergencyTagStyle label="Sự kiện khẩn cấp"></EmergencyTagStyle>}
+              <Stack direction="row" sx={{ marginTop: '10px' }}>
+                <StatusTagStyle label={detailData?.status} />
+                {detailData?.isEmergency && <EmergencyTagStyle label="Sự kiện khẩn cấp" />}
+              </Stack>
+
+              <Box>{detailData?.description ? parse(`${detailData?.description}`) : 'Chưa cập nhật mô tả'}</Box>
+            </Box>
+          ) : (
+            <Stack spacing={1}>
+              <Skeleton variant="text" width="30%" />
+              <Skeleton variant="text" width="10%" />
+              <Skeleton variant="text" width="100%" />
+              <Skeleton variant="text" width="60%" />
             </Stack>
+          )}
 
-            <Box>{detailData?.description ? parse(`${detailData?.description}`) : 'Chưa cập nhật mô tả'}</Box>
-          </Box>
-
-          <Grid rowSpacing={3} container>
-            <InfoItemWithIconStyle lg={6} xs={12} item>
-              <Stack className="info-item">
-                <Box className="info-item_icon">
-                  <Icon icon="solid-location-pin" className="info-item_icon_item" />
-                </Box>
-                <Box sx={{ width: '90%' }}>
-                  {detailData?.eventType === 'Sự kiện lưu động' ? (
-                    <>
-                      <Typography className="info-item_title">Khu vực lấy máu</Typography>
-                      <Typography>
-                        {detailData?.eventLocations[0]?.location.name ??
-                          detailData?.area
-                            .map((item) => {
-                              return item?.districtName;
-                            })
-                            .join(', ')
-                            .concat(' - ', detailData?.area[0]?.provinceName)}
-                      </Typography>
-                    </>
-                  ) : (
-                    <>
-                      <Typography className="info-item_title">
-                        {detailData?.eventLocations[0]?.location.name || ''}
-                      </Typography>
-                      <Typography>{detailData?.eventLocations[0]?.location.address || ''}</Typography>
-                    </>
-                  )}
-                </Box>
-              </Stack>
-            </InfoItemWithIconStyle>
-
-            <InfoItemWithIconStyle lg={6} xs={12} item>
-              <Stack className="info-item">
-                <Box className="info-item_icon">
-                  <Icon icon="solid-calendar-star" className="info-item_icon_item" />
-                </Box>
-                <Box>
-                  {isStartAndEndDateIsSame(detailData?.startDate, detailData?.endDate) ? (
-                    <Typography className="info-item_title">{`${formatDate(detailData?.startDate, 3)}`}</Typography>
-                  ) : (
-                    <Typography className="info-item_title">{`${formatDate(detailData?.startDate, 3)} - ${formatDate(
-                      detailData?.endDate,
-                      3
-                    )}`}</Typography>
-                  )}
-
-                  <Typography>{`${moment(detailData?.workingTimeStart, 'HH:mm').format('HH:mm')} - ${moment(
-                    detailData?.workingTimeEnd,
-                    'HH:mm'
-                  ).format('HH:mm')} `}</Typography>
-                </Box>
-              </Stack>
-            </InfoItemWithIconStyle>
-
-            <InfoItemWithIconStyle lg={6} xs={12} item>
-              <Stack className="info-item">
-                <Box className="info-item_icon">
-                  <Icon icon="solid-droplet" className="info-item_icon_item" />
-                </Box>
-                <Box>
-                  <Typography className="info-item_title">Nhóm máu cần lấy</Typography>
-                  <Box>
-                    {detailData?.bloodTypeNeed
-                      ? detailData?.bloodTypeNeed.map((e, i) => (
-                          <Chip
-                            key={i}
-                            label={convertBloodTypeLabel(e.bloodTypeId, e.isRhNegative)}
-                            sx={{ marginLeft: '5px' }}
-                            variant="contained"
-                            color="primary"
-                          />
-                        ))
-                      : 'Tất cả nhóm máu'}
+          {detailData ? (
+            <Grid rowSpacing={3} container>
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-location-pin" className="info-item_icon_item" />
                   </Box>
-                </Box>
-              </Stack>
-            </InfoItemWithIconStyle>
+                  <Box sx={{ width: '90%' }}>
+                    {detailData?.eventType === 'Sự kiện lưu động' ? (
+                      <>
+                        <Typography className="info-item_title">Khu vực lấy máu</Typography>
+                        <Typography>
+                          {detailData?.eventLocations[0]?.location.name ??
+                            detailData?.area
+                              .map((item) => {
+                                return item?.districtName;
+                              })
+                              .join(', ')
+                              .concat(' - ', detailData?.area[0]?.provinceName)}
+                        </Typography>
+                      </>
+                    ) : (
+                      <>
+                        <Typography className="info-item_title">
+                          {detailData?.eventLocations[0]?.location.name || ''}
+                        </Typography>
+                        <Typography>{detailData?.eventLocations[0]?.location.address || ''}</Typography>
+                      </>
+                    )}
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
 
-            <InfoItemWithIconStyle lg={6} xs={12} item>
-              <Stack className="info-item">
-                <Box className="info-item_avt">
-                  <img src={detailData?.hospital.avatarUrl} alt="ảnh đại diện" />
-                </Box>
-                <Box>
-                  <Typography className="info-item_title">Đơn vị tổ chức</Typography>
-                  <Typography>{detailData?.hospital.name}</Typography>
-                </Box>
-              </Stack>
-            </InfoItemWithIconStyle>
-          </Grid>
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-calendar-star" className="info-item_icon_item" />
+                  </Box>
+                  <Box>
+                    {isStartAndEndDateIsSame(detailData?.startDate, detailData?.endDate) ? (
+                      <Typography className="info-item_title">{`${formatDate(detailData?.startDate, 3)}`}</Typography>
+                    ) : (
+                      <Typography className="info-item_title">{`${formatDate(detailData?.startDate, 3)} - ${formatDate(
+                        detailData?.endDate,
+                        3
+                      )}`}</Typography>
+                    )}
+
+                    <Typography>{`${moment(detailData?.workingTimeStart, 'HH:mm').format('HH:mm')} - ${moment(
+                      detailData?.workingTimeEnd,
+                      'HH:mm'
+                    ).format('HH:mm')} `}</Typography>
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-droplet" className="info-item_icon_item" />
+                  </Box>
+                  <Box>
+                    <Typography className="info-item_title">Nhóm máu cần lấy</Typography>
+                    <Box>
+                      {detailData?.bloodTypeNeed
+                        ? detailData?.bloodTypeNeed.map((e, i) => (
+                            <Chip
+                              key={i}
+                              label={convertBloodTypeLabel(e.bloodTypeId, e.isRhNegative)}
+                              sx={{ marginLeft: '5px' }}
+                              variant="contained"
+                              color="primary"
+                            />
+                          ))
+                        : 'Tất cả nhóm máu'}
+                    </Box>
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_avt">
+                    <img src={detailData?.hospital.avatarUrl} alt="ảnh đại diện" />
+                  </Box>
+                  <Box>
+                    <Typography className="info-item_title">Đơn vị tổ chức</Typography>
+                    <Typography>{detailData?.hospital.name}</Typography>
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+            </Grid>
+          ) : (
+            <Grid rowSpacing={3} container>
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-location-pin" className="info-item_icon_item" />
+                  </Box>
+                  <Box sx={{ width: '90%' }}>
+                    <Skeleton variant="text" width="40%" />
+                    <Skeleton variant="text" width="60%" />
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-calendar-star" className="info-item_icon_item" />
+                  </Box>
+                  <Box sx={{ width: '90%' }}>
+                    <Skeleton variant="text" width="50%" />
+                    <Skeleton variant="text" width="20%" />
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_icon">
+                    <Icon icon="solid-droplet" className="info-item_icon_item" />
+                  </Box>
+                  <Box>
+                    <Typography className="info-item_title">Nhóm máu cần lấy</Typography>
+                    <Box>
+                      <Skeleton variant="text" width="60%" />
+                    </Box>
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+
+              <InfoItemWithIconStyle lg={6} xs={12} item>
+                <Stack className="info-item">
+                  <Box className="info-item_avt">
+                    <img src={detailData?.hospital.avatarUrl} alt="ảnh đại diện" />
+                  </Box>
+                  <Box>
+                    <Typography className="info-item_title">Đơn vị tổ chức</Typography>
+                    <Skeleton variant="text" width="60%" />
+                  </Box>
+                </Stack>
+              </InfoItemWithIconStyle>
+            </Grid>
+          )}
 
           <Grid rowSpacing={3} container>
             <Grid md={4} sm={6} xs={12} item>
@@ -511,7 +583,7 @@ const EventDetailPage = () => {
 
             <Grid md={4} sm={6} xs={12} item>
               <Typography>
-                <TitleItemStyle>Loại sự kiện:</TitleItemStyle> {detailData?.eventType}
+                <TitleItemStyle>Loại sự kiện:</TitleItemStyle> {detailData?.eventType || '-'}
               </Typography>
             </Grid>
 
@@ -533,7 +605,7 @@ const EventDetailPage = () => {
 
             <Grid md={4} sm={6} xs={12} item>
               <Typography>
-                <TitleItemStyle>Số người đăng ký hiện tại:</TitleItemStyle> {detailData?.numberOfRegistration}
+                <TitleItemStyle>Số người đăng ký hiện tại:</TitleItemStyle> {detailData?.numberOfRegistration || 0}
               </Typography>
             </Grid>
           </Grid>
