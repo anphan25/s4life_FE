3cf3108b594362e757e32ec258d09983341c318c
An <sajno076@gmail.com>
2023-03-09 14:37:09 +0700

    Implement enum round 2

---
+++ src/utils/enums/EventRegistrationStatusFilterEnum.js
@@ -0,0 +1,6 @@
+export const EventRegistrationStatusFilterEnum = Object.freeze({
+  NotYetAttended: { description: 'Chưa tham gia', value: 2 },
+  Attended: { description: 'Đã tham gia', value: 3 },
+  Cancelled: { description: 'Đã hủy đăng ký', value: 1 },
+  ConditionInsufficient: { description: 'Không đủ điều kiện sức khỏe', value: 4 },
+});
